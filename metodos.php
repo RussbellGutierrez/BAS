40b95b1fd76eae8d3f8f3876181b46e7fdda471b
Russbell Gutierrez <gutierrezrussbell@gmail.com>
2019-12-06 19:41:15 -0500

    Actualizando 1.0.0

--- metodos.php
+++ metodos.php
@@ -85,7 +85,7 @@ class Metodos{
 
     function mysqli_field_name($result, $field_offset) {
         $properties = mysqli_fetch_field_direct($result, $field_offset);
-        return is_object($properties) ? $properties->name : false;
+        return is_object($properties) ? $properties->def : false;
     }
 
     function hasher($tamanho) { 
